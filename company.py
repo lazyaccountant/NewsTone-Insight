a2f832973390815409fb59f559ce345a6f02e7c3
lazyaccountant <enyocollins@gmail.com>
2023-09-11 16:10:20 +0100

    Ticker search algorithm

--- company.py
+++ company.py
@@ -1,13 +1,27 @@
+from nltk import word_tokenize, pos_tag
 import spacy
+import pandas as pd
+from type_utils import company_data, get_ticker
+
+#nltk.download('punkt')
+#nltk.download('maxent_ne_chunker')
+#nltk.download('words')
 
 nlp = spacy.load("en_core_web_sm")
 
-text = "The GSK Exit: The grim realities, the abundant opportunities"
+#text = r"Dangote Sugar, NASCON lead gainers to open September positive"
 
-doc = nlp(text)
 
-for ent in doc.ents:
-    print(ent.text, ent.label_)
+# Define a custom entity pattern
+custom_patterns = []
+for name in company_data.keys():
+    label = {"label": "ORG", "pattern": name}
+    custom_patterns.append(label)
+
+# Add the custom patterns to the pipeline
+ruler = nlp.add_pipe("entity_ruler")
+ruler.add_patterns(custom_patterns)
+
 
 def extract_company(text: str) -> str:
     doc = nlp(text)
@@ -15,8 +29,46 @@ def extract_company(text: str) -> str:
     companies = []
     for ent in doc.ents:
         if ent.label_ == "ORG":
-            companies.append(ent.text)
+            ticker = get_ticker(ent.text)
+            if ticker:
+                companies.append(ticker)
+            else:
+                companies.append(ent.text)
+
+    text = text.lower()
+    words = word_tokenize(text)
+    tagged_words = pos_tag(words)
+
+    for i, word in enumerate(tagged_words):
+        if word[-1] == "NNP":
+            
+            if i+1 < len(tagged_words) and word[-1] == tagged_words[i+1][-1]: # check if there are two consecutive noun phrases
+                name = " ".join([word[0], tagged_words[i+1][0]]) # join NNP together
+                if name not in companies:
+                    ticker = get_ticker(name)
+                    if ticker:
+                        companies.append(ticker)
+                    else:
+                        companies.append(name)
+
+            elif i+1 < len(tagged_words) and word[-1] != tagged_words[i+1][-1]: # check for single noun phrase
+                name = word[0]
+                if name not in companies:
+                    ticker = get_ticker(name)
+                    if ticker:
+                        companies.append(ticker)
+                    else:
+                        companies.append(name)
+
+    if "NGX" in companies and len(companies) > 1:
+        companies.remove("NGX")
 
     return companies
 
-#print(extract_company("FBN Holdings employee sold N28.8 million worth of shares"))
+
+
+df = pd.read_csv("headlines.csv").set_index("Date")
+
+df["company"] = df["News"].apply(extract_company)
+
+print(df)
--- type_utils.py
+++ type_utils.py
@@ -1,19 +1,39 @@
 from datetime import datetime
 import re
+import json
+from rapidfuzz import process, fuzz
 
 
+# load nigerian companies data
+with open("company_data.json", "rb") as f:
+    company_data = json.load(f)
+
+# convert date format to datetime obj
 def convert_date(date_str: str) -> datetime:
     date_str = date_str.strip()
     date_obj = datetime.strptime(date_str, "%B %d, %Y")
     return date_obj
 
 def clean_text(text: str) -> str:
-    pattern = r"\\(.*)"
+    pattern = r"\\(.*)" # regex pattern to match strings after a unicode escape character.
     text_str = re.sub(pattern, "", text)    
     return text_str
 
+# Return maximum sentiment of news headline
 def max_sentiment(sentiment: dict) -> str:
     scores = list(sentiment.values())
     for key, value in sentiment.items():
         if value == max(scores):
             return key
+
+# extract ticker from company name        
+def get_ticker(name: str) -> str:
+    choices = company_data.keys() # list of company names
+    matched_comp = process.extractOne(name, choices, scorer=fuzz.token_set_ratio, score_cutoff=80)
+
+    if matched_comp:
+        ticker = company_data[matched_comp[0]]
+        return ticker
+    
+    else:
+        return None
