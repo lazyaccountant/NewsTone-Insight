5ba0f9da3c2420b8d9102af5dafeee39ce9f1881
lazyaccountant <enyocollins@gmail.com>
2023-09-11 18:56:31 +0100

    minor changes

--- models.py
+++ models.py
@@ -1,18 +1,19 @@
 import json
 import requests
-from type_utils import max_sentiment
+from type_utils import max_sentiment, headlines
+import pandas as pd
+import time
 
 # put it in an environ shi
 API_TOKEN = "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"
-#model = "nickmuchi/deberta-v3-base-finetuned-finance-text-classification"
-model = "nickmuchi/finbert-tone-finetuned-finance-topic-classification"
 
-API_URL = f"https://api-inference.huggingface.co/models/{model}"
 headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
 
 def query(headline, api):
-    payload = {"inputs": headline}
+    time.sleep(2)
+    payload = {"inputs": headline,
+                "wait_for_model": "True"}
     data = json.dumps(payload)
     response = requests.request("POST", api, headers=headers, data=data)
     return json.loads(response.content.decode("utf-8"))
@@ -41,5 +42,3 @@ def classification(headline):
         sentiment_dict[sentiment["label"]] = sentiment["score"]
 
     return max_sentiment(sentiment_dict)
-
-print(topic(r"MTN Nigeria raises N125bn via commercial paper notes"))
