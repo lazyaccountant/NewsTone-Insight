982cf220ba0529c0e22176a139319300f71b309a
lazyaccountant <enyocollins@gmail.com>
2023-10-03 04:48:48 +0100

    removed unicode characters

--- type_utils.py
+++ type_utils.py
@@ -9,18 +9,11 @@ import pandas as pd
 with open("company_data.json", "rb") as f:
     company_data = json.load(f)
 
-headlines = pd.read_csv("headlines.csv").set_index("Date")
-
 # convert date format to datetime obj
 def convert_date(date_str: str) -> datetime:
     date_str = date_str.strip()
     date_obj = datetime.strptime(date_str, "%B %d, %Y")
-    return date_obj
-
-def clean_text(text: str) -> str:
-    pattern = r"\\(.*)" # regex pattern to match strings after a unicode escape character.
-    text_str = re.sub(pattern, "", text)    
-    return text_str
+    return datetime.strftime(date_obj, "%m/%d/%Y")
 
 # Return maximum sentiment of news headline
 def max_sentiment(sentiment: dict) -> str:
@@ -40,3 +33,8 @@ def get_ticker(name: str) -> str:
     
     else:
         return None
+    
+def remove_unicode(text: str) -> str:
+    string_encode = text.encode("ascii", "ignore")
+    string_decode = string_encode.decode()
+    return string_decode
